454f65f70d6f5250ffef9c6de987a8ee99307640
AuroraChen <AuroraChen@uitc.com.tw>
2023-10-03 14:01:03 +0800

    愛心週預告頁-新增圖片

--- src/views/activity/LoveDonationComingsoon.vue
+++ src/views/activity/LoveDonationComingsoon.vue
@@ -1,11 +1,40 @@
 <template>
 
-    <div class="d-flex flex-column align-items-center justify-content-center" style="background-color:#fbf0c4;height:80vh;">
+    <div class="d-flex flex-column align-items-center justify-content-center" style="background-color:#fce28d;">
 
-        <img class="img-fluid" src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/superpopup/newNotify.png" alt="">
+        <img class="img-fluid d-none d-lg-block" src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/previewLoveDonation/preview_KV_PC.jpg" alt="">
 
-        <router-link to="/" class="btn btn-outline-primary btn-lg mt-5" > 回首頁</router-link>
+        <img class="img-fluid d-lg-none " src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/previewLoveDonation/preview_KV_MB.jpg" alt="">
 
-    </div>
+        <router-link to="/" class="btn btn-outline-primary btn-lg  my-4 my-lg-5" > 回首頁</router-link>
+</div>
+      <div class="bg-light pb-5">
+        <div class="container "  >
+                    <div class="row justify-content-center" >
+                        <div class="col-12 col-lg-11"  >
+                            <div class="text-center my-3 ">
+                              <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/loveDonation_october/title_04_PC.png" class="img-fluid mt-3 mt-lg-6 mb-3" alt="受贈單位">
+                           </div>
+
+                            <div class="row row-cols-3 row-cols-lg-4 g-1 ">
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-01.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-02.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-03.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-04.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-05.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-06.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-07.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-08.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-09.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-10.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-11.jpg" class="img-fluid border border-1 " alt=""> </div>
+                                <div class="col d-flex align-items-center justify-content-center "> <img src="https://yesgoimages.s3.ap-northeast-1.amazonaws.com/yesgoevent/LoveDonation/logo-12.jpg" class="img-fluid border border-1 " alt=""> </div>
+
+                            </div>
+                        </div>
+                     </div>
+
+        </div>
+      </div>
 
 </template>
